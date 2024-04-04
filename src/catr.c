3ada5ce84ce72a22543fd07cef34c71391bfc445
Andre Ribeiro <andresmribeiro@gmail.com>
2024-04-04 18:19:07 +0100

    fix: memory errors

--- src/catr.c
+++ src/catr.c
@@ -4,7 +4,7 @@
 #include <limits.h>
 #include "modes.h"
 
-long getFileSize(FILE *file) {
+long get_file_size(FILE *file) {
     if (!file) {
         return -1;
     }
@@ -15,38 +15,46 @@ long getFileSize(FILE *file) {
     return size;
 }
 
-int validateParameters(long start, long end, long fileSize) {
+int is_start_before_end(long start, long end) {
+    return start <= end;
+}
+
+int validate_params(long start, long end, long fileSize) {
     // end == LONG_MAX means that the end position was not provided by the user
     // we continue and set it to the end of the file
     if (end == LONG_MAX) {
         end = fileSize;
     }
 
-    if (start <= 0 || end < 0 || start > fileSize || end > fileSize || end < start) {
+    if (end > fileSize) {
+        end = fileSize;
+    }
+
+    if (start <= 0 || end < 0 || start > fileSize || !is_start_before_end(start, end)) {
         return 0;
     }
     return 1;
 }
 
-void catrByChars(const char *filename, long start, long end, char **outputBuffer, size_t *outputSize) {
+void catr_by_chars(const char *filename, long start, long end, char **outputBuffer, size_t *outputSize) {
     *outputBuffer = NULL;
     *outputSize = 0;
 
     FILE *file = fopen(filename, "rb");
     if (!file) {
-        perror("Error opening file");
+        perror("error opening file");
         return;
     }
 
-    long fileSize = getFileSize(file);
+    long fileSize = get_file_size(file);
     if (fileSize < 0) {
-        fprintf(stderr, "Error obtaining file size.\n");
+        fprintf(stderr, "error obtaining file size.\n");
         fclose(file);
         return;
     }
 
-    if (!validateParameters(start, end, fileSize)) {
-        fprintf(stderr, "Invalid start or end position %ld - %ld.\n", start, end);
+    if (!validate_params(start, end, fileSize)) {
+        fprintf(stderr, "start char must be before end char.\n");
         fclose(file);
         return;
     }
@@ -54,7 +62,7 @@ void catrByChars(const char *filename, long start, long end, char **outputBuffer
     long length = end - start + 1;
     *outputBuffer = (char *) malloc(length + 1);
     if (!*outputBuffer) {
-        fprintf(stderr, "Memory allocation failed.\n");
+        fprintf(stderr, "memory allocation failed.\n");
         fclose(file);
         return;
     }
@@ -62,27 +70,32 @@ void catrByChars(const char *filename, long start, long end, char **outputBuffer
     fseek(file, start - 1, SEEK_SET);
     *outputSize = fread(*outputBuffer, 1, length, file);
     if (*outputSize < (size_t) length) {
-        fprintf(stderr, "Read less data than expected.\n");
+        fprintf(stderr, "read less data than expected.\n");
     }
     (*outputBuffer)[*outputSize] = '\0';
 
     fclose(file);
 }
 
-void catrByLines(const char *filename, long startLine, long endLine, char **outputBuffer, size_t *outputSize) {
+void catr_by_lines(const char *filename, long startLine, long endLine, char **outputBuffer, size_t *outputSize) {
     *outputBuffer = NULL;
     *outputSize = 0;
 
+    if (!is_start_before_end(startLine, endLine)) {
+        fprintf(stderr, "start line must be before end line.\n");
+        return;
+    }
+
     FILE *file = fopen(filename, "r");
     if (!file) {
-        perror("Error opening file");
+        perror("error opening file");
         return;
     }
 
     size_t bufferLength = 256;
     *outputBuffer = (char *) malloc(bufferLength);
     if (!*outputBuffer) {
-        fprintf(stderr, "Memory allocation failed.\n");
+        fprintf(stderr, "memory allocation failed.\n");
         fclose(file);
         return;
     }
@@ -99,7 +112,7 @@ void catrByLines(const char *filename, long startLine, long endLine, char **outp
                 bufferLength += len;
                 char *tempBuffer = (char *) realloc(*outputBuffer, bufferLength);
                 if (!tempBuffer) {
-                    fprintf(stderr, "Memory reallocation failed.\n");
+                    fprintf(stderr, "memory reallocation failed.\n");
                     free(line);
                     fclose(file);
                     free(*outputBuffer);
@@ -126,18 +139,18 @@ void catrByLines(const char *filename, long startLine, long endLine, char **outp
     }
 }
 
-void printUsage(const char *programName) {
+void print_usage(const char *programName) {
     fprintf(stderr, "Usage: %s <file> <start> <length>\n", programName);
     fprintf(stderr, "       %s -s <file> <start> <end>\n", programName);
     fprintf(stderr, "       %s -l <file> <start line> <length>\n", programName);
     fprintf(stderr, "       %s -sl <file> <start line> <end line>\n", programName);
 }
 
-int parseArguments(int argc, char *argv[], const char **filename, long *start, long *end, int *mode) {
+int parse_args(int argc, char *argv[], const char **filename, long *start, long *end, int *mode) {
     char *endptr;
 
     if (argc < 3) {
-        printUsage(argv[0]);
+        print_usage(argv[0]);
         return 0;
     }
 
@@ -147,6 +160,10 @@ int parseArguments(int argc, char *argv[], const char **filename, long *start, l
     if (strcmp(argv[1], "-s") == 0) {
         *mode = CHAR_RANGE_MODE;
         argOffset = 1;
+        if (argc <= 3 + argOffset) {
+            fprintf(stderr, "end value is required for -s option.\n");
+            return 0;
+        }
     } else if (strcmp(argv[1], "-l") == 0) {
         *mode = LINE_LENGTH_MODE;
         argOffset = 1;
@@ -159,7 +176,7 @@ int parseArguments(int argc, char *argv[], const char **filename, long *start, l
 
     *start = strtol(argv[2 + argOffset], &endptr, 10);
     if (*endptr != '\0' || *start <= 0) {
-        fprintf(stderr, "Invalid start value.\n");
+        fprintf(stderr, "invalid start value.\n");
         return 0;
     }
 
@@ -167,27 +184,46 @@ int parseArguments(int argc, char *argv[], const char **filename, long *start, l
     if (argc > 3 + argOffset) {
         *end = strtol(argv[3 + argOffset], &endptr, 10);
         if (*endptr != '\0' || *end < 0) {
-            fprintf(stderr, "Invalid end value.\n");
+            fprintf(stderr, "invalid end value.\n");
             return 0;
         }
     }
 
-    if ((*end != LONG_MAX && *mode == CHAR_LENGTH_MODE) || *mode == LINE_LENGTH_MODE) {
+    if ((*mode == CHAR_LENGTH_MODE || *mode == LINE_LENGTH_MODE) && *end != LONG_MAX) {
         *end += *start - 1;
     }
 
     return 1;
 }
 
-
 void extract(const char *filename, long start, long end, int mode) {
     char *output = NULL;
     size_t outputSize = 0;
 
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        perror("error opening file");
+        return;
+    }
+
+    long fileSize = get_file_size(file);
+    if (fileSize < 0) {
+        fprintf(stderr, "error obtaining file size.\n");
+        fclose(file);
+        return;
+    }
+
+    // Set end to fileSize for -l and default mode if end is not provided
+    if (end == LONG_MAX && (mode == LINE_LENGTH_MODE || mode == LINE_RANGE_MODE || mode == CHAR_LENGTH_MODE)) {
+        end = fileSize;
+    }
+
+    fclose(file);
+
     if (mode == CHAR_LENGTH_MODE || mode == CHAR_RANGE_MODE) {
-        catrByChars(filename, start, end, &output, &outputSize);
+        catr_by_chars(filename, start, end, &output, &outputSize);
     } else if (mode == LINE_LENGTH_MODE || mode == LINE_RANGE_MODE) {
-        catrByLines(filename, start, end, &output, &outputSize);
+        catr_by_lines(filename, start, end, &output, &outputSize);
     }
 
     if (output) {
@@ -199,7 +235,7 @@ void extract(const char *filename, long start, long end, int mode) {
                 output[outputSize + 1] = '\0';
                 outputSize++;
             } else {
-                fprintf(stderr, "Memory reallocation failed.\n");
+                fprintf(stderr, "memory reallocation failed.\n");
                 free(output);
                 return;
             }
@@ -207,18 +243,15 @@ void extract(const char *filename, long start, long end, int mode) {
 
         fwrite(output, 1, outputSize, stdout);
         free(output);
-    } else {
-        fprintf(stderr, "No output generated.\n");
     }
 }
-
 
 int main(int argc, char *argv[]) {
     const char *filename;
     long start, end;
     int mode = 0;
 
-    if (!parseArguments(argc, argv, &filename, &start, &end, &mode)) {
+    if (!parse_args(argc, argv, &filename, &start, &end, &mode)) {
         return 1;
     }
 
